e18b7cf0246861fd749b445c500278a827210b48
Austin Edwards <austintedwards@gmail.com>
2017-03-12 17:29:16 -0600

    login page inputs added

--- gratuneapp/app/login.js
+++ gratuneapp/app/login.js
@@ -1,54 +1,93 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Button,
+    TextInput
 } from 'react-native';
+
 const onButtonPress = () => {
   Alert.alert('Button has been pressed!');
 };
-const login = () => {
-  return (
 
-    <View style={styles.container}>
-    <Image
-  style={styles.image}
-  source={require('./gnote.png')}
-    />
-      <Text style={styles.welcome}>
-        Gratune
-      </Text>
-      <Button
-  onPress={onButtonPress}
-  title="Sign-In"
-  accessibilityLabel="See an informative alert"
-/>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4d5061',
-  },
-  image: {
-    flex: 1,
-    resizeMode: 'contain',
-    width:200,
-    height:200
-  },
-  welcome: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10,
-    paddingTop:-50,
-    color: '#Cdd1c4',
-  },
-});
+    const login = () => {
+        return ( <
+            View style = {
+                styles.container
+            } >
+            <
+            Image style = {
+                styles.image
+            }
+            source = {
+                require('./gnote.png')
+            }/>
+            <Text style = {
+                styles.welcome
+            } >
+            Gratune <
+            /Text>
+            <TextInput
+              style={styles.login_input}
+              placeholder="E-mail"
+            />
+            <TextInput
+              style={styles.login_input}
+              placeholder="Password"
+            />
+            <Button
+                onPress={onButtonPress}
+                title="Sign-In"
+                color='#Cdd1c4'
+              />
+              <Button
+                  onPress={onButtonPress}
+                  title="Sign-Up"
+                  color='#Cdd1c4'
+                />
+            </View>
+        );
+    }
 
-export default login;
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#4d5061',
+        },
+        image: {
+            flex: 1,
+            resizeMode: 'contain',
+            width: 200,
+        },
+        welcome: {
+            fontSize: 40,
+            textAlign: 'center',
+            color: '#Cdd1c4',
+        },
+        login_input:{
+          fontSize: 20,
+          // color: '#Cdd1c4',
+          height: 40,
+          // padding: 20,
+          justifyContent: 'center',
+          backgroundColor: '#Cdd1c4',
+
+
+        },
+        signup:{
+          color: '#Cdd1c4',
+          paddingBottom:100,
+
+        }
+
+    });
+
+    export default login;
